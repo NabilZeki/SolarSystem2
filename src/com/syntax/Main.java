775dc9efd9fa3ebe1c1808d0d83d23b8fd8ecda0
“nzeki” <nzekisyntax@gamil.com>
2022-01-29 22:15:05 -0500

    The Basic Structure is Complete

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         SolarSystem milkway = new SolarSystem();
+        milkway.sun = 1;
+        milkway.moon = 2;
+        milkway.planets = 3;
     }
 }
