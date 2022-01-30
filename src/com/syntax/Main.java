3579370085b111297a5e2f6f35d22dfe518acad0
“nzeki” <nzekisyntax@gamil.com>
2022-01-29 22:50:31 -0500

    The issues in feature Sun has been resolved

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -12,6 +12,6 @@ public class Main {
         Sun solana = new Sun();
         solana.heat = "677777j";
         solana.radius = "327687428736";
-        solana.temp = 1224566C;
+        solana.temp = "1224566C";
     }
 }
